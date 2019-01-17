f0d08580324e9b059c78cd770777859f797dbc48
Jingwei Zhang <Jingwei.Zhang@genzeon.com>
2019-01-16 21:18:30 -0500

    adding more patients into the page

--- demo-settings.js
+++ demo-settings.js
@@ -3,9 +3,35 @@ var demo = {
   patientId: "smart-1137192"
 };
 
-/*
-var demo = {
-  serviceUrl: "http://fhir.healthintersections.com.au/open",
-  patientId: "f001"
-}; 
-*/
+
+//how to load all patients data autoly?
+var demo1 = {
+  serviceUrl: "https://r2.smarthealthit.org",
+  patientId: "smart-6666001"
+};
+
+var demo2 = {
+  serviceUrl: "https://r2.smarthealthit.org",
+  patientId: "smart-4444001"
+};
+
+var demo3 = {
+  serviceUrl: "https://r2.smarthealthit.org",
+  patientId: "smart-5555001"
+};
+
+var demo4 = {
+  serviceUrl: "https://r2.smarthealthit.org",
+  patientId: "smart-5555002"
+};
+
+var demo5 = {
+  serviceUrl: "https://r2.smarthealthit.org",
+  patientId: "smart-7777705"
+};
+
+var demo6 = {
+  serviceUrl: "https://r2.smarthealthit.org",
+  patientId: "smart-7777704"
+};
+
